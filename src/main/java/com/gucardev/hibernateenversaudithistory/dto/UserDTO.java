18e8c1c63861f15fa7686f9b8fdee51b2060d420
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-05 23:03:08 +0300

    feat: dto objects created

---
+++ src/main/java/com/gucardev/hibernateenversaudithistory/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.gucardev.hibernateenversaudithistory.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+  private Long id;
+  private String name;
+  private String username;
+  private List<AddressDTO> addressesDto;
+}
