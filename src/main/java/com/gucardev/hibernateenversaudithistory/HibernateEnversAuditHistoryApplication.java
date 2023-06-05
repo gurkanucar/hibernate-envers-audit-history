6f531b783c08090a30f43f2d12c93c3c3ce6a983
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-05 20:38:41 +0300

    feat: hibernate envers dependency added

--- src/main/java/com/gucardev/hibernateenversaudithistory/HibernateEnversAuditHistoryApplication.java
+++ src/main/java/com/gucardev/hibernateenversaudithistory/HibernateEnversAuditHistoryApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HibernateEnversAuditHistoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateEnversAuditHistoryApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HibernateEnversAuditHistoryApplication.class, args);
+  }
 }
