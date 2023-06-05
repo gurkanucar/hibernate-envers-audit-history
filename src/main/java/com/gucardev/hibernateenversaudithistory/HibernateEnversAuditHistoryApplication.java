091819e3f73a1076f5a17eb6ef0c429f822b9c2d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-05 20:36:21 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/hibernateenversaudithistory/HibernateEnversAuditHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.hibernateenversaudithistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnversAuditHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEnversAuditHistoryApplication.class, args);
+	}
+
+}
