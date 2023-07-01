d6296b804ed70439b8a5d6424f7837398bb60fc0
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-01 23:20:36 +0300

    refactor: configured for tomcat

--- src/main/java/com/gucardev/hibernateenversaudithistory/HibernateEnversAuditHistoryApplication.java
+++ src/main/java/com/gucardev/hibernateenversaudithistory/HibernateEnversAuditHistoryApplication.java
@@ -1,14 +1,23 @@
 package com.gucardev.hibernateenversaudithistory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HibernateEnversAuditHistoryApplication {
+@Slf4j
+public class HibernateEnversAuditHistoryApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(HibernateEnversAuditHistoryApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(HibernateEnversAuditHistoryApplication.class);
   }
 }
